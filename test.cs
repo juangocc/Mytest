f1b63d7dd025be492b613cc4c95db2e93e94a932
Juan Gabriel Oyola Cardona <juangocc@gmail.com>
2020-05-17 23:05:19 -0500

    Archivo de prueba de csharp

---
+++ test.cs
@@ -0,0 +1,25 @@
+using System;
+namespace 
+{
+    int main(int argc, char *argv[])
+    {
+	Console.WriteLine("Hola !!!");
+	string linea;
+	linea = Console.ReadLine();
+	int numero = int.Parse(linea);
+	float numero = float.Parse(linea);
+	Console.WriteLine("Hello World !!!");
+	bool condition = true;
+	if (condition)
+	{
+	    Console.Write("Ha entrado.");
+	}
+
+	bool condition = true;
+	if (condition)
+	{
+	    Console.Write("Ha entrado.");
+	}
+	return 0;
+    }
+}
